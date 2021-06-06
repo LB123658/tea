ba289aa9e3e44f9040361f1d413f169d03f875f5
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-06-06 12:23:04 -0500

    Update finances.js

--- finances.js
+++ finances.js
@@ -20,4 +20,85 @@ var cupD = document.getElementById("cupD");
 // money section
 var funds = document.getElementById("funds");
 
-if (localStorage.getItem("money") 
+// MONEY STORAGE for account
+var money = localStorage.getItem("money");
+if (money < 10) {
+  potB.style.pointerEvents = "none";
+  potC.style.pointerEvents = "none";
+  potD.style.pointerEvents = "none";
+  boxB.style.pointerEvents = "none";
+  boxC.style.pointerEvents = "none";
+  boxD.style.pointerEvents = "none";
+  cupB.style.pointerEvents = "none";
+  cupC.style.pointerEvents = "none";
+  cupD.style.pointerEvents = "none";
+  potB.style.cursor = "not-allowed";
+  potC.style.cursor = "not-allowed";
+  potD.style.cursor = "not-allowed";
+  boxB.style.cursor = "not-allowed";
+  boxC.style.cursor = "not-allowed";
+  boxD.style.cursor = "not-allowed";
+  cupB.style.cursor = "not-allowed";
+  cupC.style.cursor = "not-allowed";
+  cupD.style.cursor = "not-allowed";
+}
+if (money < 30) {
+  potB.style.pointerEvents = "auto";
+  potC.style.pointerEvents = "none";
+  potD.style.pointerEvents = "none";
+  boxB.style.pointerEvents = "auto";
+  boxC.style.pointerEvents = "none";
+  boxD.style.pointerEvents = "none";
+  cupB.style.pointerEvents = "auto";
+  cupC.style.pointerEvents = "none";
+  cupD.style.pointerEvents = "none";
+  potB.style.cursor = "default";
+  potC.style.cursor = "not-allowed";
+  potD.style.cursor = "not-allowed";
+  boxB.style.cursor = "default";
+  boxC.style.cursor = "not-allowed";
+  boxD.style.cursor = "not-allowed";
+  cupB.style.cursor = "default";
+  cupC.style.cursor = "not-allowed";
+  cupD.style.cursor = "not-allowed";
+}
+if (money < 50) {
+  potB.style.pointerEvents = "auto";
+  potC.style.pointerEvents = "auto";
+  potD.style.pointerEvents = "none";
+  boxB.style.pointerEvents = "auto";
+  boxC.style.pointerEvents = "auto";
+  boxD.style.pointerEvents = "none";
+  cupB.style.pointerEvents = "auto";
+  cupC.style.pointerEvents = "auto";
+  cupD.style.pointerEvents = "none";
+  potB.style.cursor = "default";
+  potC.style.cursor = "default";
+  potD.style.cursor = "not-allowed";
+  boxB.style.cursor = "default";
+  boxC.style.cursor = "default";
+  boxD.style.cursor = "not-allowed";
+  cupB.style.cursor = "default";
+  cupC.style.cursor = "default";
+  cupD.style.cursor = "not-allowed";
+}
+if (money > 49) {
+  potB.style.pointerEvents = "auto";
+  potC.style.pointerEvents = "auto";
+  potD.style.pointerEvents = "auto";
+  boxB.style.pointerEvents = "auto";
+  boxC.style.pointerEvents = "auto";
+  boxD.style.pointerEvents = "auto";
+  cupB.style.pointerEvents = "auto";
+  cupC.style.pointerEvents = "auto";
+  cupD.style.pointerEvents = "auto";
+  potB.style.cursor = "default";
+  potC.style.cursor = "default";
+  potD.style.cursor = "default";
+  boxB.style.cursor = "default";
+  boxC.style.cursor = "default";
+  boxD.style.cursor = "default";
+  cupB.style.cursor = "default";
+  cupC.style.cursor = "default";
+  cupD.style.cursor = "default";
+}
